09993a31c84df07f999e4ebfdd2028cdd80cc9f4
amrit rajput <vishal179singh@gmail.com>
2026-01-14 01:27:05 +0530

    scope and closure

--- day 4/scopes.py
+++ day 4/scopes.py
@@ -7,27 +7,27 @@ func()
 
 
 x = 99
-# def func2(y):
-#     z = x+y
-#     return z
-# res = func2(1)
-# print(res)
+def func2(y):
+    z = x+y
+    return z
+res = func2(1)
+print(res)
 
-# def func3():
-#     x= 12
-#     print(x)
-# func3()   
-# print(x)    
+def func3():
+    x= 12
+    print(x)
+func3()   
+print(x)    
 
 #CLOSURES 
 
-# def f1():
-#     x = 88
-#     def f2():
-#         print(x)
-#     return f2
-# res = f1()        
-# res()
+def f1():
+    x = 88
+    def f2():
+        print(x)
+    return f2
+res = f1()        
+res()
 
 
 def outer():
